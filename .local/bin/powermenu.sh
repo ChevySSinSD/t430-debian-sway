d34394d9a535f3634748be3bbe18c6e17951f636
ChevySSinSD <ChevySSinSD@gmail.com>
2022-05-04 21:29:26 -0700

    Wofi Config

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [ -z "$@" ]; then
-    echo -en "Suspend\0icon\x1fsystem-suspend\n"
-    echo -en "Lock\0icon\x1fsystem-lock-screen\n"
-    echo -en "Logout\0icon\x1fsystem-log-out\n"
-    echo -en "Hibernate\0icon\x1fsystem-suspend-hibernate\n"
-    echo -en "Reboot\0icon\x1fsystem-reboot\n"
-    echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
-else
-    if [ "$1" = "Shutdown" ]; then
-        sudo shutdown now
-    elif [ "$1" = "Logout" ]; then
-        swaymsg exit
-    elif [ "$1" = "Reboot" ]; then
-        sudo reboot
-    elif [ "$1" = "Suspend" ]; then
-        systemctl suspend
-    elif [ "$1" = "Lock" ]; then
-        loginctl lock-session
-    elif [ "$1" = "Hibernate" ]; then
-        sudo systemctl hibernate
-    fi
-fi
+#op=$( echo -en "Suspend\0icon\x1fsystem-suspend\n Lock\0icon\x1fsystem-lock-screen\nLogout\0icon\x1fsystem-log-out\nHibernate\0icon\x1fsystem-suspend-hibernate\nReboot\0icon\x1fsystem-reboot\n Shutdown\0icon\x1fsystem-shutdown\n" | wofi -i --dmenu )
+op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -c $HOME/.config/wofi/powerMenu | awk '{print tolower($2)}' )
+#op=$( echo -en " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -c $HOME/.config/wofi/powerMenu )
+
+case $op in 
+        poweroff)
+                ;&
+        reboot)
+                ;&
+        suspend)
+                systemctl $op
+                ;;
+        lock)
+		swaylock
+                ;;
+        logout)
+                swaymsg exit
+                ;;
+esac
