177a06416cebd20b27df3ca2ad7400df1c102995
ChevySSinSD <ChevySSinSD@gmail.com>
2022-05-06 07:03:25 -0700

    Power Menu updates

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-#op=$( echo -en "Suspend\0icon\x1fsystem-suspend\n Lock\0icon\x1fsystem-lock-screen\nLogout\0icon\x1fsystem-log-out\nHibernate\0icon\x1fsystem-suspend-hibernate\nReboot\0icon\x1fsystem-reboot\n Shutdown\0icon\x1fsystem-shutdown\n" | wofi -i --dmenu )
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -c $HOME/.config/wofi/powerMenu | awk '{print tolower($2)}' )
-#op=$( echo -en " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -c $HOME/.config/wofi/powerMenu )
+op=$( echo -en " Poweroff\nﰇ Reboot\n鈴 Hibernate\n⏾ Suspend\n Lock\n Logout" | wofi -c $HOME/.config/wofi/powerMenu | awk '{print tolower($2)}' )
 
-case $op in 
-        poweroff)
-                ;&
-        reboot)
-                ;&
-        suspend)
-                systemctl $op
-                ;;
+case $op in
         lock)
-		swaylock --clock --indicator --fade-in 0.2 -i ~/Pictures/Wallp
-apers/mount-fuji.jpg
+		swaylock --clock --indicator --fade-in 0.2 -i ~/Pictures/Wallpapers/mount-fuji.jpg
                 ;;
         logout)
                 swaymsg exit
+                ;;
+	*)
+                systemctl $op
                 ;;
 esac
